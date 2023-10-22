b3e25b8c5de98bbee404ff74fa24d282f0d640b9
Anoth <mixaetel@gmail.com>
2023-10-22 12:43:35 +0300

    added other files

---
+++ Assets/Scripts/BlockScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // using TMPro;
+public class BlockScript : MonoBehaviour
+{
+    public GameObject textObject;
+    Text textComponent;
+    public int hitsToDestroy;
+    public int points;
+    PlayerScript playerScript;
+    void Start()
+    {
+        if (textObject != null)
+        {
+            textComponent = textObject.GetComponent<Text>();
+            textComponent.text = hitsToDestroy.ToString();
+        }
+        playerScript = GameObject.FindGameObjectWithTag("Player")
+                        .GetComponent<PlayerScript>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        {
+            hitsToDestroy--;
+            if (hitsToDestroy == 0)
+            {
+                Destroy(gameObject);
+                playerScript.BlockDestroyed(points);
+            }
+            else if (textComponent != null)
+                textComponent.text = hitsToDestroy.ToString();
+        }
+    }
+}
